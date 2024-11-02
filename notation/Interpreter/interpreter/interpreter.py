d981c18e3aeee1b429bf0088531fb7c46602c0ce
Evgeniy Timoffev <geka.timof.max@gmail.com>
2024-11-02 14:30:01 +0000

    add some comments to interpreter

--- notation/Interpreter/interpreter/interpreter.py
+++ notation/Interpreter/interpreter/interpreter.py
@@ -7,9 +7,9 @@ class Interpreter():
         self._text = ""
         self._current_token = None
 
-    #
+    # get current element and move
     def __next(self):
-        #
+        # end text checking
         if self._pos > len(self._text)-1:
             return Token(TokenType.EOL, "")
 
@@ -25,7 +25,7 @@ class Interpreter():
             return Token(TokenType.OPERATOR, current_char)
         raise SystemExit("bad token")
 
-    #
+    # check token type
     def __check_token(self, type_: TokenType) -> None:
         if self._current_token.type_ == type_:
             self._current_token = self.__next()
@@ -33,12 +33,16 @@ class Interpreter():
             raise SystemExit("invalid token order")
 
 
+    #
     def eval(self, s:str) -> int:
         self._text = s
         self._pos = 0
 
+        # get element
         self._current_token = self.__next()
+        # set left element
         left = self._current_token
+        # check this element
         self.__check_token(TokenType.INTEGER)
 
         op = self._current_token
